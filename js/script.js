5140e57b079c056cce846a49ff6edefe30c671e5
Yuri Marinho <ymarinho2025@gmail.com>
2025-12-04 10:17:20 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Espera o carregamento da página e redireciona
+document.addEventListener("DOMContentLoaded", function() {
+window.location.href = "https://marinhonetwork.vercel.app/marinhohub/hub.html";
+});
