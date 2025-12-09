7318ec80e8f88cac09c2bbc8203c8fd82181319a
Yuri Marinho <ymarinho2025@gmail.com>
2025-12-09 16:28:13 -0300

    Delete js directory

--- js/script.js
+++
@@ -1,6 +0,0 @@
-// Espera o carregamento da página e redireciona
-document.addEventListener("DOMContentLoaded", function() {
-window.location.href = "https://marinhonetwork.vercel.app/marinhohub/index.html";
-
-});
-
