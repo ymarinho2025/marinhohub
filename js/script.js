a3f292404af1b60e49d43f1def309dbb6ea8beb9
Yuri Marinho <ymarinho2025@gmail.com>
2025-12-08 23:50:58 -0300

    Update redirection URL on page load

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // Espera o carregamento da página e redireciona
 document.addEventListener("DOMContentLoaded", function() {
-window.location.href = "https://marinhonetwork.vercel.app/marinhohub";
+window.location.href = "https://marinhonetwork.vercel.app/marinhohub/index.html";
 
 });
+
